394d8b202bc7bd56ae372cecba52a1321e39c538
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-12-26 16:07:54 -0600

    added css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import {connect} from 'react-redux'
-import { fetchPlayers } from './actions/fetchPlayers';
-import PlayersContainer from './containers/PlayersContainer'
-import Button from './components/Button'
-import Home from './components/Home'
-
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPlayers } from "./actions/fetchPlayers";
+import PlayersContainer from "./containers/PlayersContainer";
+import Button from "./components/Button";
+import Home from "./components/Home";
 
 class App extends React.Component {
-  
-  
   render() {
     return (
       <div className="App">
         <Home />
         <PlayersContainer />
-        <Button >Button</Button>
+        {/* <Button >Button</Button> */}
       </div>
-    )
+    );
   }
-  
-
 }
 
 // const mSTP = (state) => {
@@ -28,4 +23,4 @@ class App extends React.Component {
 //   }
 // }
 
-export default connect(null, {fetchPlayers})(App);
+export default connect(null, { fetchPlayers })(App);
--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,49 +1,77 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import {addPlayer} from '../actions/addPlayer'
-import Button from '../components/Button'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addPlayer } from "../actions/addPlayer";
+import Button from "../components/Button";
 
 class PlayerInput extends Component {
+  state = {
+    name: "",
+    school: "",
+    year: "",
+    scout: "",
+  };
 
-    state = {
-        name: '',
-        school: '',
-        year: '',
-        scout: ''
-    }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleChange = (event) => {
-       this.setState({
-        [event.target.name]: event.target.value
-       })
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addPlayer(this.state);
+    this.setState({
+      name: "",
+      school: "",
+      year: "",
+      scout: "",
+    });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        this.props.addPlayer(this.state)
-        this.setState({
-            name: '',
-            school: '',
-            year: '',
-            scout: ''
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <label>Add a Player</label>
-                <form onSubmit={this.handleSubmit}>
-                    {/* <label>Add Player</label> */}
-                    <input type='text' name="name" placeholder='Name' value={this.state.name} onChange={this.handleChange}/><br/>
-                    <input type='text' name="school" placeholder='School' value={this.state.school} onChange={this.handleChange}/><br/>
-                    <input type='text' name="year" placeholder='Year' value={this.state.year} onChange={this.handleChange}/><br/>
-                    <input type='text' name="scout" placeholder='Scout' value={this.state.scout} onChange={this.handleChange}/><br/>
-                    <input type='submit'/>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {/* <label>Add a Player</label> */}
+        <br />
+        <form class="newnewnew" onSubmit={this.handleSubmit}>
+          {/* <label>Add Player</label> */}
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="school"
+            placeholder="School"
+            value={this.state.school}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="year"
+            placeholder="Year"
+            value={this.state.year}
+            onChange={this.handleChange}
+          />
+          <br />
+          <textarea
+            type="text"
+            name="scout"
+            placeholder="Scout"
+            value={this.state.scout}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input class="btn-two" type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default connect(null, {addPlayer})(PlayerInput);
+export default connect(null, { addPlayer })(PlayerInput);
